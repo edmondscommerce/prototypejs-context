941abd49ff01ac77db2c8f29a0f8186f0304db0e
Dan <dan@edmondscommerce.co.uk>
2016-01-15 12:44:17 +0000

    Updated readme and formatting

--- src/PrototypeJsContext.php
+++ src/PrototypeJsContext.php
@@ -1,17 +1,9 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: michael
- * Date: 20/02/15
- * Time: 17:17
- */
-
-namespace EdmondsCommerce\PrototypeJsContext;
+<?php namespace EdmondsCommerce\PrototypeJsContext;
 
 class PrototypeJsContext extends Behat\MinkExtension\Context\MinkContext implements Context, SnippetAcceptingContext
 {
     /**
-     *@Then /^I wait for AJAX to finish$/
+     * @Then /^I wait for AJAX to finish$/
      */
     public function iWaitForAjaxToFinish()
     {
